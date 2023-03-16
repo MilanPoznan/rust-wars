57a88f046ff82971b6eccce4f4e88aec1a272f71
Milan Poznan <milan@jl.is>
2023-03-16 16:43:29 +0100

    first commint

---
+++ src/main.rs
@@ -0,0 +1,9 @@
+fn reverse_words(str: &str) -> String {
+    // your code here
+    let word_vec = str.split(" ");
+    for word in word_vec.into_iter() {}
+}
+
+fn main() {
+    println!("Hello, world!");
+}
